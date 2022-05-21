b7da902cd42875f27e544d3fabeba413091522f9
harriszhang <harriszhang@live.cn>
2022-05-21 19:51:03 +0800

    1.2.0

---
+++ Panosen.Math.MSTest/BinaryTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panosen.Math.MSTest
+{
+    [TestClass]
+    public class BinaryTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var @base = 2;
+
+            var nums = Binary.ToBase(0, @base);
+
+            var actual = Binary.FromBase(nums, @base);
+
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var @base = 2;
+
+            var nums = new List<int> { 0, 0, 0 };
+
+            var actual = Binary.FromBase(nums, @base);
+
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            var ramdom = new Random();
+
+            var @base = ramdom.Next(7, 23);
+
+            var value = ramdom.Next(100000, 10000000);
+
+            var nums = Binary.ToBase(value, @base);
+
+            var actual = Binary.FromBase(nums, @base);
+
+            Assert.AreEqual(value, actual);
+        }
+    }
+}
---
+++ Panosen.Math/Binary.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panosen.Math
+{
+    /// <summary>
+    /// 任意进制转换
+    /// </summary>
+    public static class Binary
+    {
+        /// <summary>
+        /// 十进制转换为任意进制
+        /// </summary>
+        public static List<int> ToBase(int num, int @base)
+        {
+            if (num == 0)
+            {
+                return new List<int> { 0 };
+            }
+
+            List<int> items = new List<int>();
+            while (num > 0)
+            {
+                items.Add(num % @base);
+                num /= @base;
+            }
+
+            items.Reverse();
+
+            return items;
+        }
+
+        /// <summary>
+        /// 任意进制转换为十进制
+        /// </summary>
+        public static int FromBase(List<int> nums, int @base)
+        {
+            if (nums == null || nums.Count == 0)
+            {
+                return 0;
+            }
+
+            int value = 0;
+            for (int index = 0; index < nums.Count; index++)
+            {
+                value += nums[index];
+                if (index < nums.Count - 1)
+                {
+                    value *= @base;
+                }
+            }
+
+            return value;
+        }
+    }
+}
